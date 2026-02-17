fd9194e6e1a81616f1ef560af10230d785c30e55
fuzail <fuzail.mirza@nightingaletech.net>
2026-02-17 17:33:53 +0500

    Added CORS for dev

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://college-admin-portal-amber.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
