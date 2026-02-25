4a5955969003f6d9e37bce4aed94cb37859dd7a7
fuzail <fuzail.mirza@nightingaletech.net>
2026-02-25 16:03:24 +0500

    New port added in CORS

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:5173",
         "https://college-admin-portal-amber.vercel.app",
     ],
     allow_credentials=True,
